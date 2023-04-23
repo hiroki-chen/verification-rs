857d76430d6754f7359f150dee7f6f6759b60115
Haobin Chen <etyyuiope@qq.com>
2023-04-22 23:43:20 -0400

    Add examples for creusot

--- creusot/src/lib.rs
+++ creusot/src/lib.rs
@@ -1,9 +1,76 @@
-use creusot_contracts::*;
+#[cfg_attr(not(creusot), proc_macro_hygiene)]
+#[cfg_attr(not(creusot), stmt_expr_attributes)]
+use creusot_contracts::{logic::*, *};
 
-#[ensures((^vec).deep_model().sorted())]
-pub fn sort<T>(vec: &mut Vec<T>)
+#[predicate]
+fn sorted_range<T: OrdLogic>(s: Seq<T>, l: Int, u: Int) -> bool {
+    pearlite! {
+        forall<i : Int, j : Int> l <= i && i < j && j < u ==> s[i] <= s[j]
+    }
+}
+
+#[predicate]
+fn sorted<T: OrdLogic>(s: Seq<T>) -> bool {
+    sorted_range(s, 0, s.len())
+}
+
+#[requires(v.deep_model().sorted())]
+#[ensures((^v).deep_model().sorted())]
+pub fn insert_in_order<T>(v: &mut Vec<T>, item: T)
 where
     T: Ord + DeepModel,
     T::DeepModelTy: OrdLogic,
 {
+    let mut idx = 0;
+    if v.last() <= Some(&item) {
+        idx = v.len();
+    } else {
+        #[invariant(prev, forall<i: Int> i >= 0 && i < produced.len() ==>
+            v.deep_model()[i] <= item.deep_model()
+        )]
+        for i in 0..v.len() {
+            if v[i] > item {
+                idx = i;
+                break;
+            }
+        }
+    }
+
+    v.insert(idx, item);
+}
+
+#[predicate]
+fn partition<T: OrdLogic>(v: Seq<T>, i: Int) -> bool {
+    pearlite! { forall<k1 : Int, k2: Int> 0 <= k1 && k1 < i && i <= k2 && k2 < v.len() ==> v[k1] <= v[k2]}
+}
+
+#[ensures(sorted((^v).deep_model()))]
+#[ensures((^v)@.permutation_of(v@))]
+pub fn selection_sort<T: Ord + DeepModel>(v: &mut Vec<T>)
+where
+    T::DeepModelTy: OrdLogic,
+{
+    let old_v = ghost! { v };
+    #[invariant(permutation, v@.permutation_of(old_v@))]
+    #[invariant(sorted, sorted_range(v.deep_model(), 0, produced.len()))]
+    #[invariant(partition, partition(v.deep_model(), produced.len()))]
+    for i in 0..v.len() {
+        let mut min = i;
+        #[invariant(min_is_min, forall<k: Int> i@ <= k && k < produced.len() + i@ + 1 ==> v.deep_model()[min@] <= v.deep_model()[k])]
+        #[invariant(min_bound, i@ <= min@ && min@ < produced.len() + i@ + 1)]
+        for j in (i + 1)..v.len() {
+            if v[j] < v[min] {
+                min = j;
+            }
+        }
+        v.swap(i, min);
+    }
+}
+
+#[cfg(not(creusot))]
+#[test]
+fn test() {
+    let mut v = ::std::vec![6, 5, 4, 3, 2, 1];
+    selection_sort(&mut v);
+    println!("{v:?}");
 }
